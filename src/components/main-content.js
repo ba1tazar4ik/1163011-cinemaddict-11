1db689db3450ec78d2fa39ee6fb5c79c10fd755a
ba1tazar4ik <ba1tazar4ik@gmail.com>
2020-05-21 02:18:45 +0300

    refactor main-content.js:  extends AbstractComponen

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import AbstractComponent from "./abstract-component.js";
 
 const createMainContentTemplate = () => {
   return (
@@ -14,24 +14,12 @@ const createMainContentTemplate = () => {
   );
 };
 
-export default class MainContent {
+export default class MainContent extends AbstractComponent {
   constructor() {
-    this._element = null;
+    super();
   }
 
   getTemplate() {
     return createMainContentTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
